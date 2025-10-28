d0ab41696b542aa727d5d4f2643cb8463ea77790
nguyenyou <tunt081295@gmail.com>
2025-10-28 17:54:07 +0700

    first commit

---
+++ demo.scala
@@ -0,0 +1,15 @@
+//> using scala 3.7.3
+//> using platform scala-js
+
+//> using dep "org.scala-js::scalajs-dom::2.8.1"
+//> using dep "com.raquo::laminar::17.2.1"
+
+import org.scalajs.dom
+
+import com.raquo.laminar.api.L.*
+
+@main
+def run(): Unit = {
+  val container = dom.document.getElementById("app")
+  render(container, div("Hello World!"))
+}
